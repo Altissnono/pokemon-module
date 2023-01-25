e6b9b350fa0dc3efa2be2142cd16d45219655537
altissnono <altissnono@my-email.space>
2023-01-25 17:02:21 +0100

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put, Query, Render } from '@nestjs/common';
+import { PokemonService } from './services/pokemon.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly pokemonservice: PokemonService) {}
   @Get()
   @Render('index')
-  root(): { message: string; } {
+  root(): any {
     return { message: 'Hello world! je suis un kiwi !' };
+  }
+
+  @Get()
+  Getallpokemon() {
+    return this.pokemonservice.getAllPokemon();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { PokemonService } from './services/pokemon.service';
+import { PokemonController } from './controllers/PokemonController';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [PokemonController],
+  providers: [PokemonService],
 })
 export class AppModule {}
