ac5eb62885fdf4b584818abe71e1809fa40403e0
altissnono <altissnono@my-email.space>
2023-01-26 11:33:11 +0100

    WORK - CRASH - UPDATE FIX NOT WORKING

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
-import { PokemonList } from 'src/domains/pokemonlist';
+import { PokemonList } from 'src/domains/pokemonList';
 import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Injectable()
@@ -19,7 +19,33 @@ export class PokemonService {
         },
       }),
     );
-    console.log(typeof data);
-    return data;
-  }
+
+    // create an array to store the Pokemon data
+    let pokemons = [];
+
+    // iterate over the results and make a call to the specific Pokemon endpoint
+    for (const pokemon of data.results) {
+        const { data: pokemonData } = await firstValueFrom(
+            this.httpService.get(pokemon.url, {
+                responseType: 'json',
+                headers: {
+                    'Accept-Encoding': 'gzip,deflate,compress',
+                },
+            })
+        );
+        let description = '';
+        if(pokemonData.flavor_text_entries && pokemonData.flavor_text_entries.length>0)
+        {
+            description = pokemonData.flavor_text_entries[0].flavor_text;
+        }
+        // push the pokemon data including image and description
+        pokemons.push({
+            name: pokemon.name,
+            image: pokemonData.sprites.front_default,
+            description: description
+        });
+    }
+
+    return pokemons;
+}
 }
